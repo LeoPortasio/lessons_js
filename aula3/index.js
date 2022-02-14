6dbdf5fbb6e273b95c7a96e5d6d2c6a5c8e51365
Leo Portasio <leoportasio@gmail.com>
2022-02-14 16:48:38 -0300

    adicionando as primeiras aulas do curso JS e criando o arquivo .gitignore

---
+++ aula3/index.js
@@ -0,0 +1,7 @@
+console.log('Hello word');
+console.log("Hello 'word'");
+console.log(`'leo'`);
+// console.log mostra o valor(string) e quebra a linha
+
+console.log(123456, 15.85, 'Leo');
+// tudo relacionado a nºs é number - ponto flutuante, inteiro, real, etc
