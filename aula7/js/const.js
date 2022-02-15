f6cb7bf004896ea239a5ab3a0754769d049069cc
Leo Portasio <leoportasio@gmail.com>
2022-02-15 10:20:59 -0300

    aula7 - constante

---
+++ aula7/js/const.js
@@ -0,0 +1,37 @@
+// nunca criar constantes com palavras reservadas
+
+// criar constantes com nomes significativos
+
+// nunca criar constantes começadas com nºs
+
+// utilizar letras minúsculas
+
+//não pode conter espaços ou traços
+
+//nome composto utilzamos camelCase
+
+// constantes são Case-sensitive
+
+// não podemos modificar o valor das constantes
+
+// não utiliza var, utilize const
+
+const nome = 'João';
+console.log(nome);
+
+// a constante precisar ser inicializada, ou seja,
+// colocamos o valor ao declará-la
+
+// não tem como alterar o valor da constante
+
+const numberOne = 5;
+const numberTwo = 10;
+const conta = numberOne * numberTwo;
+let resultado = conta * 3;
+resultado = resultado + 5;
+console.log(resultado);
+
+console.log(typeof (numberOne));
+// descobre o tipo da variável numbreOne
+
+//para concatenar devemos utilizar + com uma string, se for nºs o js soma
