050889e46af10d8459a2a1c951997fa73eef2103
Leo Portasio <leoportasio@gmail.com>
2022-02-16 10:14:45 -0300

    aula8 - let x var

---
+++ aula8/js/let_x_var.js
@@ -0,0 +1,13 @@
+var nome = 'Leo';
+var nome = 'Portasio'
+// com o var é sobrescrita a variável nome, o que não acontece com o let
+console.log(nome);
+//não gera erro porque sobrescreveu
+
+/* 
+NUNCA CRIAR VARIÁVEIS SEM ESPECIFICAR LET OU VAR
+JavaScript aceita criar a variável sem especificar let ou var.
+NÃO FAÇA ISSO!!!
+*/
+
+
