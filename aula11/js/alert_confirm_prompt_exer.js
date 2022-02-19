891d9e25772c9a999bfc66f11bbfbfb071306d43
Leo Portasio <leoportasio@gmail.com>
2022-02-19 05:15:14 -0300

    diminuimos o tamanho do codigo, entretanto, deixando implicito o resultado com a soma de duas variaveis

--- aula11/js/alert_confirm_prompt_exer.js
+++ aula11/js/alert_confirm_prompt_exer.js
@@ -2,5 +2,8 @@ let num1 = prompt('Digite o 1º nº');
 let num2 = prompt('Digite o 2º nº');
 num1 = Number(num1);
 num2 = Number(num2);
-const resultado = num1 + num2;
-alert('O resultado da soma foi: ' + resultado);
+// const resultado = num1 + num2;
+alert(`O resultado da soma foi: ${num1 + num2}`);
+
+// em códigos complexos é de boa prática deixar explícito o que queremos fazer
+// ou seja, definir a const resultado e chamá-la
