2b6656cccc6337bf2d7c328dd812a41ece47b21e
Leo Portasio <leoportasio@gmail.com>
2022-03-21 14:04:55 -0300

    aula20 - operadores de comparaçao

---
+++ aula20/js/oper_comparacao.js
@@ -0,0 +1,33 @@
+/*
+
+
+OPERADORES DE COMPARAÇÃO
+
+> maior que
+>= maior que ou igual a
+< menor que
+<= menor que ou igual a
+== igualdade(valor) *************
+!= diferente(valor) *************
+=== igualdade estrita (valor e tipo)
+!== diferente estrito (valor e tipo)
+
+
+*/
+const expressao = 10 > 5; // true
+console.log(expressao);
+
+const expressao1 = 10 >= 11; // false
+console.log(expressao1);
+
+const expressao2 = 10 < 15; // true
+console.log(expressao2);
+
+const expressao3 = 10 <= 9; // false
+console.log(expressao3);
+
+const num1 = 10;
+const num2 = 10;
+const expressao4 = num1 == num2;
+console.log(expressao4);
+
