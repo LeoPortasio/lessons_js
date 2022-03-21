0f1eb831ca15c9cd74c913bb42010e2644f62825
Leo Portasio <leoportasio@gmail.com>
2022-03-21 14:29:57 -0300

    aula21 - operadores logicos

---
+++ aula21/js/oper_logicos.js
@@ -0,0 +1,22 @@
+/*
+
+OPERADORES LÓGICOS
+
+&&  -> AND -> E
+
+||  -> OR  -> OU
+
+!   -> NOT -> NÃO
+
+*/
+
+console.log(true && true);
+console.log(true || false);
+console.log(!true);
+
+const usuario = 'Leo';
+const senha = '123456';
+const logado = usuario === 'Leo' && senha === '123456';
+console.log(logado);
+
+
