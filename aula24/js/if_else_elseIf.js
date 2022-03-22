0eb8baf690eed5436c929972a53a9d16cd9214b8
Leo Portasio <leoportasio@gmail.com>
2022-03-22 05:16:11 -0300

    aula24 - if - else - else if

---
+++ aula24/js/if_else_elseIf.js
@@ -0,0 +1,16 @@
+const hora = 23;
+
+if (hora >= 0 && hora < 12) {
+    console.log('Bom dia');
+}
+else if (hora >= 12 && hora <= 18) {
+    console.log('Boa tarde')
+}
+else if (hora > 18 && hora < 24) {
+    console.log('Boa noite')
+}
+else {
+    console.log('Horário inválido');
+}
+
+
