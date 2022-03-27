1800798b27d9b72b6e4fc00b6b1199fde9241252
Leo Portasio <leoportasio@gmail.com>
2022-03-27 06:49:39 -0300

    correcao aula35 DOM

--- aula35/js/dom.js
+++ aula35/js/dom.js
@@ -1,8 +1,14 @@
+// DOM - Interface de objetos que permite manipular elemento no html ou css
+// utilizamos js para manipular os objetos da API do DOM
+// sempre que falamos do DOM estamos nos referindo ao browser
+
 // windows --> objeto pai do DOM
 
-// window.document --> documento HTML --> árvore do DOM - child de window
+// window.document --> documento HTML --> árvore do DOM - child de window - parent de html
 
-//  html --> child de document
+//  html --> child de document - parent de head - body
 
 // head - body --> child de html
+
+
 
